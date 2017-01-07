d19bc2d06c5e3035d729a144348f91036b4421e5
Krzysztof Janosz <janosz.krzysztof@gmail.com>
2017-01-07 23:14:03 +0100

    Add base common settings for nixos

---
+++ nixos/common.nix
@@ -0,0 +1,29 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    <nixpkgs/nixos/modules/installer/scan/not-detected.nix>
+  ];
+
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+
+  i18n = {
+    consoleFont = "Lat2-Terminus16";
+    consoleKeyMap = "pl";
+    defaultLocale = "en_DK.UTF-8";
+  };
+  
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+  };
+
+  users.defaultUserShell = pkgs.zsh;
+
+  environment.systemPackages = with pkgs; [
+    coreutils
+    emacs
+    git
+  ];
+}
